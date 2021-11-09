acff5f149f6756c4ac5e71a75634cbaab8fdacac
Alex <alex@alexswiec.com>
2021-11-09 19:23:29 +0100

    changed user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -19,7 +19,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await use6187078aa3822110a915a625r.generateAuthToken()
+        console.log(user)
+        const token = await user.generateAuthToken()
         res.send({user, token})
     } catch (e) {
         res.status(400).send()
